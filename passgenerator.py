83ee9cfa99736ca93bdcac84b5187794234f7390
Sujeettiwari001 <152081663+Sujeettiwari001@users.noreply.github.com>
2023-11-29 17:20:48 +0530

    Add files via upload

---
+++ passgenerator.py
@@ -0,0 +1,14 @@
+import string
+import random
+s1=string.ascii_lowercase
+s2=string.ascii_uppercase
+s3=string.digits
+s4=string.punctuation
+len=int(input("Enter the length of password:"))
+s=[]
+s.extend(list(s1))
+s.extend(list(s2))
+s.extend(list(s3))
+s.extend(list(s4))
+print("Your password is:")
+print("".join(random.sample(s,len)))
